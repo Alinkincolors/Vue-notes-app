2965b192f08371d3f44feaf12d7580c0dee62aca
Alina <alina@MacBook-Pro-Alina-2.local>
2020-06-26 15:53:13 +0300

    Initiate commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default"></div>
+        <div class="panel-body quote">
+            <slot></slot>
+        </div>
+
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    .panel-body {
+        font-family: 'Indie Flower', cursive;
+        font-size: 24px;
+        color: gray;
+    }
+
+    .panel {
+        border-color: grey;
+        margin: 0px;
+
+    }
+
+   .quote {
+       cursor: pointer;
+   }
+
+   .quote:hover {
+       background-color: blanchedalmond ;
+   }
+</style>
